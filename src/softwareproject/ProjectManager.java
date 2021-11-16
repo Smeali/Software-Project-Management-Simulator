dc562004693475c55876050cba463748d541a2b9
Eli Conley <elijahtconley@gmail.com>
2021-11-15 22:57:02 -0500

    fixed wording and added comments section A of the requirements is complete; still needs testing

--- src/softwareproject/ProjectManager.java
+++ src/softwareproject/ProjectManager.java
@@ -1,4 +1,6 @@
-package softwareproject; //package name, idk if this matters
+package softwareproject;
+
+//Section created by Eli C. on 11/15/2021
 
 import java.util.ArrayList; //importing arraylist
 import java.util.Scanner; //importing scanner class
@@ -6,19 +8,18 @@ import java.util.Scanner; //importing scanner class
 public class ProjectManager {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in); //create scanner
-        boolean running = true; //while loop controller
-        ArrayList<String> members = new ArrayList<String>(); //team members list
-        ArrayList<String> risk = new ArrayList<String>();; //risk list
-        ArrayList<String> status = new ArrayList<String>();; //risk status list
+        ArrayList<String> members = new ArrayList<>(); //team members list
+        ArrayList<String> risk = new ArrayList<>(); //risk list
+        ArrayList<String> status = new ArrayList<>(); //risk status list
         String input; //stores user input in a string
 
-        System.out.println("Enter owner or project name: "); //owner or project name
+        System.out.println("Enter owner or project manager name: "); //owner or project manager name
         String name = scan.nextLine(); //name string
 
         System.out.println("Enter description: "); //description of software project
         String desc = scan.nextLine(); //description string
 
-        while(running) { //allows as many team members as needed
+        while(true) { //allows as many team members as needed
             System.out.println("Enter project team member (enter stop to stop): ");
             input = scan.nextLine();
             if (input.equals("stop") || input.equals("Stop")) {
@@ -27,7 +28,7 @@ public class ProjectManager {
             members.add(input); //add one team member
         }
 
-        while(running) { //inputting risk and risk status
+        while(true) { //inputting risk and risk status
             System.out.println("Enter a risk (enter stop to stop): ");
             input = scan.nextLine();
             if(input.equals("stop") || input.equals("Stop") ) {
@@ -45,16 +46,18 @@ public class ProjectManager {
         System.out.println("Name: " + name);
         System.out.println("Desc: " + desc);
 
-        System.out.print("Team members: ");
+        System.out.print("Team members: "); //prints team members as a list
         for(int i = 0; i < members.size()-1; i++) {
             System.out.print(members.get(i) + ", ");
         }
         System.out.print(members.get(members.size()-1));
-        System.out.println("");
+        System.out.println(""); //adds a space line
 
-        for(int j = 0; j < risk.size(); j++) {
+        for(int j = 0; j < risk.size(); j++) { //prints risk and status (one of each per line)
             System.out.println("Risk: " + risk.get(j) + ", Status: " + status.get(j));
         }
         System.out.println("------------------------------------------------------------");
+
+        //add more stuff here
     }
 }
